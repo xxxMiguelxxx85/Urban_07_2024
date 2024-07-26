ad8b257572593e6da38215536b503c209bf0e3b8
xxxMiguelxxx85 <vjecki@mail.ru>
2024-07-27 00:15:25 +0500

    Add files via upload

---
+++ homework_26072024/variable.py
@@ -0,0 +1,5 @@
+Number_of_completed_homework = '12'
+Number_of_hours_spent = '1.5'
+Course_name = "Python"
+Time_for_one_task = (1.5 / 12)
+print('Курс:',Course_name,', всего задач:',Number_of_completed_homework,', затрачено часов:',Number_of_hours_spent,', среднее время выполнения', Time_for_one_task,'.')

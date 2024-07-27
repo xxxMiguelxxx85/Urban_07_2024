60375dd50d822b16d2b8f5c54d8b936abd25d6cc
xxxMiguelxxx85 <vjecki@mail.ru>
2024-07-27 12:56:31 +0500

    Update homework2.py

--- homework_26072024/homework2.py
+++ homework_26072024/homework2.py
@@ -1,5 +1,5 @@
-Number_of_completed_homework = '12'
-Number_of_hours_spent = '1.5'
+Number_of_completed_homework = 12
+Number_of_hours_spent = 1.5
 Course_name = "Python"
-Time_for_one_task = (1.5 / 12)
+Time_for_one_task = (Number_of_hours_spent / Number_of_completed_homework)
 print('Курс:',Course_name,', всего задач:',Number_of_completed_homework,', затрачено часов:',Number_of_hours_spent,', среднее время выполнения', Time_for_one_task,'.')

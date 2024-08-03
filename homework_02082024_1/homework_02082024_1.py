8a596b131494e725eb9c1fa65d7aa11d726acba1
xxxMiguelxxx85 <vjecki@mail.ru>
2024-08-04 00:29:18 +0500

    Update homework_02082024_1.py

--- homework_02082024_1/homework_02082024_1.py
+++ homework_02082024_1/homework_02082024_1.py
@@ -2,9 +2,9 @@ first = int(input("Введите первое число: "))
 second = int(input("Введите второе число: "))
 third = int(input("Введите третье число: "))
 
-if first == second and second == third and third == first:
+if first == second and second == third and third == first: # - Если все числа равны между собой, то вывести 3
     print(3)
-elif first == second or second == third or first == third:
+elif first == second or second == third or third == first: # - Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
     print(2)
 else:
-    print(0)
+    print(0) # - Если равных чисел среди 3-х вообще нет, то вывести 0

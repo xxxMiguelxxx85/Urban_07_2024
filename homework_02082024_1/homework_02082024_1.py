cda53201d65dd2aa699914e5a5e6a38c28941490
xxxMiguelxxx85 <vjecki@mail.ru>
2024-08-04 00:25:39 +0500

    Add files via upload

---
+++ homework_02082024_1/homework_02082024_1.py
@@ -0,0 +1,10 @@
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+third = int(input("Введите третье число: "))
+
+if first == second and second == third and third == first:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
